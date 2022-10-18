79f5aaf6121befc4a23dac3fc4e7bf8fb1fff0e4
barbaraech <barbaraech9@gmail.com>
2022-10-17 23:54:14 -0300

    agrega opciones de navegador e inicio de chrome

--- ScrapingGoogleFlights.py
+++ ScrapingGoogleFlights.py
@@ -7,4 +7,18 @@ from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 
-#Opciones de navgeacion
+#Opciones de navegacion
+options = webdriver.ChromeOptions()
+#modo incognito
+options.add_argument('--incognito')
+#inicia maximizado
+options.add_argument('--start-maximized')
+#deshabilita extensiones
+#options.add_argument('--disable-extensions')
+
+driver_path = 'C:\\Users\\BARBARA\\Desktop\\AutomationSearchFlights\\chromedriver'
+
+driver = webdriver.Chrome(driver_path, chrome_options=options)
+
+#Inicia navegador
+driver.get('https://www.google.com/flights?hl=es')
