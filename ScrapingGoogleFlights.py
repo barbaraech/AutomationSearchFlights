8d242bf7ec0fdbfa1601cd746e83e7fc08f98db7
barbaraech <barbaraech9@gmail.com>
2022-10-17 22:44:34 -0300

    Creacion proyecto

---
+++ ScrapingGoogleFlights.py
@@ -0,0 +1,8 @@
+#Librerias
+from selenium import webdriver
+#Para realizar operacion hasta que todos los elementos de html carguen
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+import time
+
