02b0dd29f3e50d924a9bdb00994eecae454b2929
barbaraech <barbaraech9@gmail.com>
2022-10-18 21:28:37 -0300

    incorpora click en boton ida y vuelta

--- ScrapingGoogleFlights.py
+++ ScrapingGoogleFlights.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 #Para realizar operacion hasta que todos los elementos de html carguen
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
@@ -22,3 +22,10 @@ driver = webdriver.Chrome(driver_path, chrome_options=options)
 
 #Inicia navegador
 driver.get('https://www.google.com/flights?hl=es')
+
+#Click en ida/vuelta
+#en 5 seg si no encuentra elemento se detiene programa
+WebDriverWait(driver, 5)\
+    .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+    'button.VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-INsAgc VfPpkd-LgbsSe-OWXEXe-Bz112c-UbuQg VfPpkd-LgbsSe-OWXEXe-dgl2Hf Rj2Mlf OLiIxf PDpWxe LQeN7 BobFtf'.replace(' ','.'))))\
+        .click()
